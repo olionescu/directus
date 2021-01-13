7505e7278430e47ec4aaa51325c4add12a1ac8db
WoLfulus <wolfulus@gmail.com>
2021-01-13 15:33:21 -0300

    Change emitAsync.catch to use the emitAsyncSafe function

--- api/src/services/items.ts
+++ api/src/services/items.ts
@@ -14,8 +14,7 @@ import {
 } from '../types';
 import Knex from 'knex';
 import cache from '../cache';
-import emitter from '../emitter';
-import logger from '../logger';
+import emitter, { emitAsyncSafe } from '../emitter';
 import { toArray } from '../utils/to-array';
 import env from '../env';
 
@@ -162,17 +161,15 @@ export class ItemsService<Item extends AnyItem = AnyItem> implements AbstractSer
 			return primaryKeys;
 		});
 
-		emitter
-			.emitAsync(`${this.eventScope}.create`, {
-				event: `${this.eventScope}.create`,
-				accountability: this.accountability,
-				collection: this.collection,
-				item: savedPrimaryKeys,
-				action: 'create',
-				payload: payloads,
-				schema: this.schema,
-			})
-			.catch((err) => logger.warn(err));
+		emitAsyncSafe(`${this.eventScope}.create`, {
+			event: `${this.eventScope}.create`,
+			accountability: this.accountability,
+			collection: this.collection,
+			item: savedPrimaryKeys,
+			action: 'create',
+			payload: payloads,
+			schema: this.schema,
+		});
 
 		return Array.isArray(data) ? savedPrimaryKeys : savedPrimaryKeys[0];
 	}
@@ -354,17 +351,15 @@ export class ItemsService<Item extends AnyItem = AnyItem> implements AbstractSer
 				await cache.clear();
 			}
 
-			emitter
-				.emitAsync(`${this.eventScope}.update`, {
-					event: `${this.eventScope}.update`,
-					accountability: this.accountability,
-					collection: this.collection,
-					item: key,
-					action: 'update',
-					payload,
-					schema: this.schema,
-				})
-				.catch((err) => logger.warn(err));
+			emitAsyncSafe(`${this.eventScope}.update`, {
+				event: `${this.eventScope}.update`,
+				accountability: this.accountability,
+				collection: this.collection,
+				item: key,
+				action: 'update',
+				payload,
+				schema: this.schema,
+			});
 
 			return key;
 		}
@@ -491,17 +486,15 @@ export class ItemsService<Item extends AnyItem = AnyItem> implements AbstractSer
 			await cache.clear();
 		}
 
-		emitter
-			.emitAsync(`${this.eventScope}.delete`, {
-				event: `${this.eventScope}.delete`,
-				accountability: this.accountability,
-				collection: this.collection,
-				item: keys,
-				action: 'delete',
-				payload: null,
-				schema: this.schema,
-			})
-			.catch((err) => logger.warn(err));
+		emitAsyncSafe(`${this.eventScope}.delete`, {
+			event: `${this.eventScope}.delete`,
+			accountability: this.accountability,
+			collection: this.collection,
+			item: keys,
+			action: 'delete',
+			payload: null,
+			schema: this.schema,
+		});
 
 		return key;
 	}
